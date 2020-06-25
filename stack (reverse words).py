db5621c5e19ef049cf731690b5ad226a758ef347
shishir kumar thakur <52493461+shishir7654@users.noreply.github.com>
2020-06-25 07:51:39 +0530

    Add files via upload

---
+++ stack (reverse words).py
@@ -0,0 +1,24 @@
+def  reverseWords(string):
+    st = list()
+
+
+    for i in range(len(string)):
+        if string[i] != " ":
+            st.append(string[i])
+
+
+        else:
+            while len(st)>0:
+                print(st[-1],end = "")
+                st.pop()
+
+            print(end = " ")
+
+    while len(st)>0:
+        print(st[-1], end = "")
+        st.pop()
+
+
+if __name__ =="__main__":
+    string = " this is coder"
+    reverseWords(string)
