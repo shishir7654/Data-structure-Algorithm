feab91c226b7a227767230ae097fba60db8f24b7
shishir kumar thakur <52493461+shishir7654@users.noreply.github.com>
2020-06-25 07:37:11 +0530

    Add files via upload

---
+++ Stack(Delete middle ele).py
@@ -0,0 +1,15 @@
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items ==[]
+
+    def push(self, item):
+        self.item.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def peek(self):
+        return
---
+++ string(duck number).py
@@ -0,0 +1,23 @@
+def checkduck(num) : 
+  
+    # Length of the number(number of digits) 
+    n = len(num)  
+     
+    # Ignore leading 0s 
+    i = 0
+    while (i < n and num[i] == '0') : 
+        i = i + 1
+  
+    # Check remaining digits 
+    while (i < n) :  
+        if (num[i] == "0") : 
+            return True 
+        i = i + 1
+  
+    return False 
+
+num1 = "12023"
+if(checkduck(num1)):
+    print("It is a duck number")
+else:
+    print("It is not a duck number")
